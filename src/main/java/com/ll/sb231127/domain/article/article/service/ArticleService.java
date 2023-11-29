82fe2eb99e74aaed1ee126b17c8bcf9ac39e39d2
jae9380 <ljy5314@gmail.com>
2023-11-29 14:10:45 +0900

    step2

--- src/main/java/com/ll/sb231127/domain/article/article/service/ArticleService.java
+++ src/main/java/com/ll/sb231127/domain/article/article/service/ArticleService.java
@@ -5,6 +5,8 @@ import com.ll.sb231127.domain.article.article.repository.ArticleRepository;
 import com.ll.sb231127.domain.member.member.entity.Member;
 import com.ll.sb231127.global.rsData.RsData;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -49,4 +51,9 @@ public class ArticleService {
 
     public void addTags(String tagContent) {
     }
+
+    public Page<Article> search(Pageable pageable) {
+        return articleRepository.findAll(pageable);
+    }
+
 }
